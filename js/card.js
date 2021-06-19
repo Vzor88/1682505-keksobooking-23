f321a460021f7ec8d35bba2461ec38243e4f157c
Vzor <S0k0linA@yandex.ru>
2021-06-19 21:18:39 +0300

    сделал описание всех dom элементов

--- js/card.js
+++ js/card.js
@@ -1,12 +1,1 @@
-import {setAdverts} from './data';
-
-const simularCardList = document.querySelector('.map__canvas');
-const simularAdvertTemplate = document.querySelector('#card').content.querySelector('.popup');
-
-
-const simularAdvert = setAdverts();
-simularAdvert.forEach(() => {
-  const simularItemCard = simularAdvertTemplate.cloneNode(true);
-  simularCardList.appendChild(simularItemCard);
-});
 
--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 import {getRandInt, getRandFloat} from './util.js';
 
 const TITLES = [
-  'Продаю отличную квартиру!',
+  'Сдаётся номер для молодожёнов',
   'Сдам дом в аренду',
   'Продам малогабаритную квартиру',
   'Продам дедушкин дом',
@@ -32,6 +32,14 @@ const TYPES = [
   'house',
   'bungalow',
   'hotel',
+];
+
+const TYPES_RUS = [
+  'Дворец',
+  'Квартира',
+  'Дом',
+  'Бунгало',
+  'Отель',
 ];
 
 const FIXED_TIMES = [
@@ -74,10 +82,10 @@ const ROOMS = {
 
 const GUESTS = {
   MIN: 1,
-  MAX: 100,
+  MAX: 10,
 };
 
-const ADVERT_COUNT = 10;
+const ADVERT_COUNT = 2;
 const FLOATING_POINT = 5;
 
 function getRandArray(array) {
@@ -93,6 +101,51 @@ function getRandomElementFromArray(array) {
   return array[getRandInt (0, array.length -1)];
 }
 
+function getRandRooms(count) {
+  let descriptionRooms = `${count} комнат`;
+  if(count === 1) {
+    descriptionRooms = `${count} комната`;
+  }
+  if(count > 2 && count < 5 ) {
+    descriptionRooms = `${count} комнаты`;
+  }
+  return descriptionRooms;
+}
+
+function getRandGuests(count) {
+  let descriptionGuests = `${count} гостей`;
+  if(count === 1) {
+    descriptionGuests = `${count} гостя`;
+  }
+  return descriptionGuests;
+}
+
+function getLangRus(array, arrayRus) {
+  return arrayRus[getRandInt (0, array.length -1)];
+}
+
+function getDisplayFeaturesList(array, itemList) {
+  const modifiers = array.map((feature) => `popup__feature--${feature}`);
+  itemList.querySelectorAll('.popup__feature').forEach((element) => {
+    const modfier = element.classList[1];
+    if(!modifiers.includes(modfier)) {
+      element.remove();
+    }
+  });
+  return modifiers;
+}
+
+function getDisplayPhotosList (array, itemList){
+  const photosItem = itemList.querySelector('.popup__photo');
+  photosItem.remove();
+  for(let item = 0; item < array.length; item++) {
+    const photosElement = photosItem.cloneNode(true);
+    photosElement.src = array[item];
+    itemList.appendChild(photosElement);
+  }
+}
+
+
 const createAdvert = function (index) {
   const randomLatitude = getRandFloat(LOCATIONS.LATITUDE_MIN, LOCATIONS.LATITUDE_MAX, FLOATING_POINT);
   const randomLongitude = getRandFloat(LOCATIONS.LONGITUDE_MIN, LOCATIONS.LONGITUDE_MAX, FLOATING_POINT);
@@ -104,7 +157,7 @@ const createAdvert = function (index) {
       title: getRandomElementFromArray(TITLES),
       address: `${randomLatitude}, ${randomLongitude}`,
       price: getRandInt (PRICE.MIN, PRICE.MAX),
-      type: getRandomElementFromArray(TYPES),
+      type: getLangRus(TYPES, TYPES_RUS),
       rooms: getRandInt (ROOMS.MIN, ROOMS.MAX),
       guests: getRandInt (GUESTS.MIN, GUESTS.MAX),
       checkin: getRandomElementFromArray(FIXED_TIMES),
@@ -123,3 +176,22 @@ const createAdvert = function (index) {
 const setAdverts = () => new Array(ADVERT_COUNT).fill(null).map((value,index) => createAdvert(index + 1));
 
 export {setAdverts};
+
+const simularCardList = document.querySelector('.map__canvas');
+const simularAdvertTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const simularAdvert = setAdverts();
+simularAdvert.forEach((advert) => {
+  const simularItemCard = simularAdvertTemplate.cloneNode(true);
+  simularItemCard.querySelector('.popup__avatar').src = advert.author.avatar;
+  simularItemCard.querySelector('.popup__title').textContent = advert.offer.title;
+  simularItemCard.querySelector('.popup__text--address').textContent = advert.offer.address;
+  simularItemCard.querySelector('.popup__text--price').textContent = `${advert.offer.price} ₽/ночь`;
+  simularItemCard.querySelector('.popup__type').textContent = advert.offer.type;
+  simularItemCard.querySelector('.popup__text--capacity').textContent = `${getRandRooms(advert.offer.rooms)}  для ${getRandGuests(advert.offer.guests)}`;
+  simularItemCard.querySelector('.popup__text--time').textContent = `Заезд после ${advert.offer.checkin}, выезд до ${advert.offer.checkout}`;
+  getDisplayFeaturesList(advert.offer.features, simularItemCard.querySelector('.popup__features'));
+  simularCardList.appendChild(simularItemCard);
+  simularItemCard.querySelector('.popup__description').textContent = advert.offer.description;
+  getDisplayPhotosList(advert.offer.photos, simularItemCard.querySelector('.popup__photos'));
+});
