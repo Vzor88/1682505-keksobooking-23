26614dc7662e6549405217dd83ca800d4aa88ed8
Vzor <S0k0linA@yandex.ru>
2021-07-02 10:32:49 +0300

    Теперь данные берутся с сервера, удалил все ранее используемые данные и необходимые к ним функции

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ import './activate-form.js';
 import './map.js';
 import './card.js';
 import './form.js';
+import {renderAdvertList} from './card.js';
 
+fetch('https://23.javascript.pages.academy/keksobooking/data')
+  .then((response) => response.json())
+  .then((adverts) => {
+    renderAdvertList(adverts);
+  });
